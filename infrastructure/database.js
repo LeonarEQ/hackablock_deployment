3c22796dd12600b655e0f134304c4d92e23474b5
LeonarEQ <leonardestupinan@yahoo.com>
2022-02-21 00:42:19 +0100

    nueva variable

--- infrastructure/database.js
+++ infrastructure/database.js
@@ -1,6 +1,6 @@
 const mysql = require('mysql2/promise');
 
-const { DATABASE_HOST, DATABASE_NAME, DATABASE_USER, DATABASE_PASSWORD } =
+const { DATABASE_HOST, DATABASE_NAME, DATABASE_USER, DATABASE_PASSWORD, DATABASE_PORT } =
   process.env;
 
 const pool = mysql.createPool({
@@ -8,6 +8,7 @@ const pool = mysql.createPool({
   database: DATABASE_NAME,
   user: DATABASE_USER,
   password: DATABASE_PASSWORD,
+  port: DATABASE_PORT,
 });
 
 module.exports = { pool };
